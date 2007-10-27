2bd28901d16757bbc3458ae87fc19f2cfdff6393
daniel.kummer <daniel.kummer@90ac9a91-763d-0410-b516-e32d82a4ccde>
2007-10-27 12:44:16 +0000

    all result displays in one window

--- matlab/imclean.m
+++ matlab/imclean.m
@@ -1,4 +1,6 @@
 function [res_img] = imclean(img)
-
 %filterung (median / minmax)
-res_img = img;
+
+%TEST: sharpening
+laplacianfilter = [0,-1,0; -1, 1+4, -1; 0,-1,0];
+res_img = imfilter(img, laplacianfilter, 'replicate');
--- matlab/imconvert.m
+++ matlab/imconvert.m
@@ -1,12 +1,7 @@
 function [res_img] = imconvert(tmp, th)
 %später vielleicht automatischer threshhold mit historamm
 
-%TEST: sharpening
-%laplacianfilter = [0,-1,0; -1, 1+4, -1; 0,-1,0];
-%tmp = imfilter(tmp, laplacianfilter, 'replicate');
-
-res_img = tmp;
-
+%res_img = tmp;
 tmp = double(tmp);
 img_max = max(max(max(tmp)));
 tmp = tmp / img_max;
@@ -20,5 +15,3 @@ for i=1:w-1
         end
     end
 end
-figure(333)
-imshow(tmp, [])
