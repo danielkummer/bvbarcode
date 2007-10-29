14d565d0e7f350495fafd1e0ec996116cd93ccf0
daniel.kummer <daniel.kummer@90ac9a91-763d-0410-b516-e32d82a4ccde>
2007-10-29 15:41:10 +0000

    ...

--- trunk/java/BV-Barcode/src/bvbarcode/Main.java
+++ trunk/java/BV-Barcode/src/bvbarcode/Main.java
@@ -15,47 +15,30 @@ import img.*;
  */
 public class Main {
     
-    /** Creates a new instance of Main */
-    
-    /**
-     * @param args the command line arguments
-     */
+    private static BarcodeProcessor bc;
+
     public static void main(String[] args) {
-boolean flag = true;
+        boolean running = true;
 
+        bc = BarcodeProcessor.getInstance();
+        
 	// instantiate image im1 and attach a frame grabber	
-	Img im1 = new Img(new ImgGrabber("preview"));
-	
+	Img image = new Img(new ImgGrabber("preview"));
+        Img result = new Img(image.width, image.height, Img.GRAY);
 	// grab one frame to get size, etc. 
-	im1.grabFrame();
-
-	// instantiate image im2 with the same dimensions as im1 and set to
-	// type gray scale
-	Img im2 = new Img(im1.width, im1.height, Img.GRAY);
-
+	image.grabFrame();
 	// position the windows on the screen
-        im1.setSingleWindowDisplay(300,10);
- 	im2.setSingleWindowDisplay(300,350);
-	
-
+        image.setSingleWindowDisplay(300,10);
+        result.setSingleWindowDisplay(350,350);
+ 	
 	// start grabbing frames from the cam
-	while (flag) {
-		
+	while (running) {
 		// wait for 10ms (max 100 frames per sec)
 		Img.sleep(10);
-
-		im1.grabFrame();
-			
-		for (int i = 1; i < im1.height-1; i++) {
-			for (int j = 1; j < im1.width-1; j++) {
-				im2.pix[i][j].c0 = (short)(((int)(im1.pix[i][j].c0 + 
-									im1.pix[i][j].c1 + im1.pix[i][j].c2))/3);
-			}
-		}
-
-
-		im1.display();	// display (update) image im1
-		im2.display();	// display (update) image im1
+    		image.grabFrame();
+                result = bc.ImgToGrayScale(image);
+                result.display();
+		image.display();
 	}
-}
+    }
 }
